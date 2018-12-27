fe5b4b0c63f83eaab897766e670b8d0b7bd5edb7
양동우 <ckck803@naver.com>
2018-12-27 11:29:13 +0900

    수정 및 프로그램 추가

--- Brewfile
+++ Brewfile
@@ -3,6 +3,7 @@ tap "homebrew/cask"
 tap "homebrew/cask-fonts"
 tap "homebrew/core"
 tap "homebrew/services"
+tap "caskroom/fonts"
 
 # 중요! 이 줄이 없으면, ~/Applications에 설치하므로 꼭 넣어야 한다.
 cask_args appdir: '/Applications'
@@ -14,6 +15,9 @@ brew 'wget'
 brew 'tmux'
 brew 'git'
 brew 'mackup'
+brew 'neovim'
+brew 'fzf'
+
 
 # Applications
 cask 'font-hack-nerd-font'
@@ -21,4 +25,9 @@ cask 'istat-menus'
 cask 'iterm2'
 cask 'google-chrome'
 cask 'visual-studio-code'
-
+cask 'android-studio'
+cask 'slack'
+cask 'karabiner-elements'
+cask 'intellij-idea-ce'
+cask 'pycharm-ce'
+cask 'parallels13'
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if test ! $(which brew); then
-  ruby-e "$(curl -fsSL https://raw.gitgubusercontents.com/Homebrew/install/master/install)"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew update
@@ -17,10 +17,26 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 [ ! -f $HOME/.zshrc ] && ln -nfs $HOME/dotfiles/.zshrc $HOME/.zshrc
 source $HOME/.zshrc
 
+#powerlevel9k 설치
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+#pure prompt
+brew install nodejs
+npm install --global pure-prompt
+
+#zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+#zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#spacevim
+curl -sLf https://spacevim.org/install.sh | bash
+
 [ ! -f $HOME/.mackup.cfg ] && ln -nfs $HOME/dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
-/usr/local/bin/composer global require laravel/installer laravel/envoy laravel/valet tightenco/jigsaw
-$HOME/.composer/vendor/bin/valet install
-git clone git@github.com:laravel/homestead.git $HOME/Homestead
-[ $(basename $(pwd)) == "Homestead" ] && cd $HOME/Homestead
-vagrant box add laravel/homestead
+#/usr/local/bin/composer global require laravel/installer laravel/envoy laravel/valet tightenco/jigsaw
+#$HOME/.composer/vendor/bin/valet install
+#git clone git@github.com:laravel/homestead.git $HOME/Homestead
+#[ $(basename $(pwd)) == "Homestead" ] && cd $HOME/Homestead
+#vagrant box add laravel/homestead
