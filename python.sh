d55e6f74d5e37f36283e6c452332c01152f6b860
양동우 <ckck803@naver.com>
2019-05-07 02:12:56 +0900

    python.sh 수정

--- python.sh
+++ python.sh
@@ -4,5 +4,5 @@ cd $HOME
 pip3 install virtualenv
 virtualenv env1
 source ./env1/bin/activate
-pip3 install --upgrade jupyter matplotlib numpy pandas scipy sckit-learn
+pip3 install --upgrade jupyter matplotlib numpy pandas scipy scikit-learn
 
